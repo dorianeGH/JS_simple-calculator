c61ffe2158d9ef981345130728e6a49dccb0a495
Doriane <berdalado@gmail.com>
2021-03-09 14:34:00 +0100

    step2 prompt

--- calculator.js
+++ calculator.js
@@ -1,1 +1,12 @@
 console.log('Hello World')
+
+const firstValue = prompt('what is the first value ?')
+const operator = prompt('what is the operator ?')
+const secondValue =prompt('what is the second value ?')
+
+console.log(firstValue)
+console.log(operator)
+console.log(secondValue)
+
+
+
