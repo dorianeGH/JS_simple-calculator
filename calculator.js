de298f64de37c153e42c8b1a7c8e342108427344
Doriane <berdalado@gmail.com>
2021-03-09 15:20:53 +0100

    step 5

--- calculator.js
+++ calculator.js
@@ -4,12 +4,27 @@ const firstValue = parseInt(prompt('what is the first value ?'));
 const operator = prompt('what is the operator ?')
 const secondValue = parseInt(prompt('what is the second value ?'));
 
-if (operator === "+"){
+/*if (operator === "+"){
     console.log(firstValue + secondValue)
 }
 else {
     console.log(firstValue - secondValue)
+}*/
+
+switch(operator){
+      case "+":
+        cconsole.log(firstValue + secondValue);
+        break;
+    case "-":
+        console.log(firstValue - secondValue);
+        break;
+    case "*":
+        cconsole.log(firstValue * secondValue);
+        break;
+    case "/":
+        cconsole.log(firstValue / secondValue);
+        break;
+    default:
+        console.log("invalid operator");
+        break;
 }
-
-
-
