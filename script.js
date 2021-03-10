ae84446def4267f5482c0d594bac23f2a9fc0ec6
Doriane <berdalado@gmail.com>
2021-03-10 16:05:37 +0100

    add GetElement before correction

--- script.js
+++ script.js
@@ -1,31 +1,17 @@
-console.log('Hello World')
+//const premierNombre = parseInt(prompt('Entrez premiere valeur'));
+const operateur = prompt("entrez l'operateur");
+//const secondNombre = parseInt(prompt("entrez une seconde valeur"));
 
-/*const player = prompt('what is your name dear player ?')
+let resultat = "Inconnu";
 
-const rightPrice = Math.floor(Math.random() * 100) + 1;
-//console.log(rightPrice)
-let n = prompt('your turn to guess the right price ?')
-
-while (n !== rightPrice){
-    n > rightPrice console.log('it is less'); 
-    n < rightPrice console.log('it is more');
-    n == rightPrice console.log(player + 'wins');
-}*/
-
-const playerName = parseInt(prompt('whats your name ?'));
-const randomNumber = Math.floor(Math.random() * 100) + 1;
-
-let playserHasWon = false
-
-while (!playserHasWon) {
-    const playerGuess = parseInt(prompt ('whats your guess ?'));
-
- if (playerGuess < randomNumber){
-    alert('more');
-} else if (playerGuess > randomNumber){
-    alert('less');
-} else if (playerGuess === randomNumber){
-    alert (playerName + ' wins');  
-playserHasWon = true; 
+if (operateur === "+"){
+    resultat = parseInt(document.getElementById('userResponseFirst').value) + parseInt(document.getElementById('userResponseSecond').value);
+} else if (operateur ==="-"){
+    resultat = parseInt(document.getElementById('userResponseFirst').value) - parseInt(document.getElementById('userResponseSecond').value);
+} else if (operateur ==="*"){
+    resultat = parseInt(document.getElementById('userResponseFirst').value) * parseInt(document.getElementById('userResponseSecond').value);
+} else if (operateur ==="/"){
+    resultat = parseInt(document.getElementById('userResponseFirst').value) / parseInt(document.getElementById('userResponseSecond').value);
 }
-}
+
+alert (resultat);
