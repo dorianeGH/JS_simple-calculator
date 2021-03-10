4d45da24ed5de6dd36e6b6d5795b78e589200886
Doriane <berdalado@gmail.com>
2021-03-10 16:37:07 +0100

    get Element with right correction

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //const premierNombre = parseInt(prompt('Entrez premiere valeur'));
-const operateur = prompt("entrez l'operateur");
+/*const operateur = prompt("entrez l'operateur");
 //const secondNombre = parseInt(prompt("entrez une seconde valeur"));
 
 let resultat = "Inconnu";
@@ -12,6 +12,32 @@ if (operateur === "+"){
     resultat = parseInt(document.getElementById('userResponseFirst').value) * parseInt(document.getElementById('userResponseSecond').value);
 } else if (operateur ==="/"){
     resultat = parseInt(document.getElementById('userResponseFirst').value) / parseInt(document.getElementById('userResponseSecond').value);
+}
+alert (resultat);*/
+
+//Exo 2 : 
+//on aurait pu :
+//const userResponseFirst = parseInt(document.getElementById('userResponseFirst').value);
+//const userResponseSecond = parseInt(document.getElementById('userResponseSecond').value);
+
+const userResponseFirst = document.getElementById('userResponseFirst');
+const userResponseSecond = document.getElementById('userResponseSecond');
+const ope = document.getElementById('operateur');
+
+const premierNombre = parseInt(userResponseFirst.value);
+const secondNombre = parseInt(userResponseSecond.value);
+const operateur = ope.value;
+
+let resultat = "Inconnu";
+
+if (operateur === "+"){
+    resultat = premierNombre + secondNombre;
+} else if (operateur ==="-"){
+    resultat = premierNombre - secondNombre;
+} else if (operateur ==="*"){
+    resultat = premierNombre * secondNombre;
+} else if (operateur ==="/"){
+    resultat = premierNombre / secondNombre;
 }
 
 alert (resultat);
